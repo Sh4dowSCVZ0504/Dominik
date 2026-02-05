4235fb31c11f42c6f7024d084494526869e49d12
Sh4dow <riplonely071@gmail.com>
2026-02-05 20:48:56 -0300

    Update main.js

--- tower-defense/js/main.js
+++ tower-defense/js/main.js
@@ -1,10 +1,35 @@
 const canvas = document.getElementById("game");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = "#111";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+const enemy = {
+  x: -40,
+  y: canvas.height / 2 - 20,
+  size: 40,
+  speed: 1.5
+};
 
-ctx.fillStyle = "#0f0";
-ctx.font = "20px Arial";
-ctx.fillText("Tower Defense iniciado 🚀", 20, 40);
+function update() {
+  enemy.x += enemy.speed;
 
+  if (enemy.x > canvas.width) {
+    enemy.x = -enemy.size;
+  }
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "#111";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+  ctx.fillStyle = "red";
+  ctx.fillRect(enemy.x, enemy.y, enemy.size, enemy.size);
+}
+
+function loop() {
+  update();
+  draw();
+  requestAnimationFrame(loop);
+}
+
+loop();
